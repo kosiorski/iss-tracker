808e6995ec439e913ec7c261c0d459e9061d416d
k0sa <kosiorski.tomasz@gmail.com>
2019-03-09 15:23:58 +0100

    Added IssPosition and tests (TDD).

---
+++ src/main/java/pl/kosiorski/isstracker/model/IssPosition.java
@@ -0,0 +1,12 @@
+package pl.kosiorski.isstracker.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IssPosition {
+
+  private double latitude;
+  private double longitude;
+}
