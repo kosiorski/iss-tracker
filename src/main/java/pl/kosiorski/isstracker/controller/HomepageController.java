6f6b6d3c22e9a2850d89f0794a867b8a752a0eb6
k0sa <kosiorski.tomasz@gmail.com>
2019-03-10 12:29:55 +0100

    Working on google map api.

---
+++ src/main/java/pl/kosiorski/isstracker/controller/HomepageController.java
@@ -0,0 +1,16 @@
+package pl.kosiorski.isstracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomepageController {
+
+  @GetMapping
+  public String showHomepage() {
+
+    return "/index";
+  }
+}
