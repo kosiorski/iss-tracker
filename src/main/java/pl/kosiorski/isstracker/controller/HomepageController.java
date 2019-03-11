897202e6acd7d8c5373241c2363616e6e88e9fe6
k0sa <kosiorski.tomasz@gmail.com>
2019-03-11 17:25:47 +0100

    Working on google map api.

--- src/main/java/pl/kosiorski/isstracker/controller/HomepageController.java
+++ src/main/java/pl/kosiorski/isstracker/controller/HomepageController.java
@@ -1,39 +1,15 @@
 package pl.kosiorski.isstracker.controller;
 
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-import pl.kosiorski.isstracker.model.IssData;
-import pl.kosiorski.isstracker.service.IssDataService;
 
 @Controller
 @RequestMapping("/home")
 public class HomepageController {
 
-  private final IssDataService issDataService;
-
-  @Autowired
-  public HomepageController(IssDataService issDataService) {
-    this.issDataService = issDataService;
-  }
-
   @GetMapping
-  public String showHomepage(Model model) {
-
-    //      model.addAttribute("firstPosition", new Position(51.1757, -65.2334));
-
-    final String url = "http://api.open-notify.org/iss-now.json";
-
-    RestTemplate restTemplate = new RestTemplate();
-    String json = restTemplate.getForObject(url, String.class);
-
-    IssData issData = new Gson().fromJson(json, IssData.class);
-
-    model.addAttribute("currentPosition", issData.getIss_position());
+  public String showHomepage() {
 
     return "/index";
   }
